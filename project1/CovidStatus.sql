c1bc29bd140bd193ae813c47f9470dd7a5e0096b
Trong D. Pham <70780829+phdtrong@users.noreply.github.com>
2023-08-19 15:25:34 -0700

    Update CovidStatus.sql

--- project1/CovidStatus.sql
+++ project1/CovidStatus.sql
@@ -4,6 +4,9 @@
 --2. Visit SQL and select DatabaseName > Task > Import Data(*)
 --  Excel version: Microsoft Excel 97-2003
 --  Source: Select the data in step 1 to import into your DatabaseName
+--  Destination: Microsoft OLE DB Provider for SQL Server
+--  Server Name: Ensure your server name is correctly selected
+--  Database: Ensure your database name is correctly selected (i.e. DatabaseName above)
 --  (*)Issue of SQL Server Excel Import - The 'Microsoft.ACE.OLEDB.12.0' ? 
 --   solution is download and then install 32bit version of importer tool as show here
 --   https://download.microsoft.com/download/2/4/3/24375141-E08D-4803-AB0E-10F2E3A07AAA/AccessDatabaseEngine.exe
