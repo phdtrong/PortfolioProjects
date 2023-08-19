4108e787373099a2bece72e719aff72bbfbbda13
Trong D. Pham <70780829+phdtrong@users.noreply.github.com>
2023-08-19 15:03:42 -0700

    Update CovidStatus.sql
    Provide solution for --  Error of importer tool in SQL

---
+++ project1/CovidStatus.sql
@@ -0,0 +1,13 @@
+-- Project 1. SQL DATA EXPLORATION
+--1. Visit ourworldindata.org/covid-deaths to download, 
+--  split data into 2 separate files as CovidDeaths.xlsx and CovidVaccinations.xlsx
+--2. Visit SQL and select DatabaseName > Task > Import Data(*)
+--  Excel version: Microsoft Excel 97-2003
+--  Source: Select the data in step 1 to import into your DatabaseName
+--  (*)Issue of SQL Server Excel Import - The 'Microsoft.ACE.OLEDB.12.0' ? 
+--   solution is download and then install 32bit version of importer tool as show in this folder
+--3. Apply SQL techniques to clean, filter data and finally create views for step 4
+
+
+
+--4. Apply Tableau to visualize views' data that are given from step 3
