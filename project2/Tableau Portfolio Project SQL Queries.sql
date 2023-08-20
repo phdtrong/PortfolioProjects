f5fe109d60f09527145e93f6ad179db9b8b69548
Trong D. Pham <70780829+phdtrong@users.noreply.github.com>
2023-08-20 16:36:04 -0700

    Update Tableau Portfolio Project SQL Queries.sql

--- project2/Tableau Portfolio Project SQL Queries.sql
+++ project2/Tableau Portfolio Project SQL Queries.sql
@@ -4,20 +4,16 @@ Query statements are from project 1 (SQL - Data Exploration)
 Main goal is to install, use, apply Public Tableau version for data visualization
 */
 
+-- A. Prepare data
 
-
--- 1. death, case counts and death over case rate per date
-
-Select SUM(new_cases) as total_cases, SUM(cast(new_deaths as int)) as total_deaths, SUM(cast(new_deaths as int))/SUM(New_Cases)*100 as DeathPercentage
-From PortfolioProject..CovidDeaths
---Where location like '%states%'
+-- 1. Global View of total cases, total deaths, and deaths over cases rate
+select SUM(new_cases) as total_cases, SUM(cast(new_deaths as int)) as total_deaths, SUM(cast(new_deaths as int))/SUM(New_Cases)*100 as DeathPercentage
+from SQL_Tutorial..CovidDeaths$
 where continent is not null 
---Group By date
 order by 1,2
 
 -- Just a double check based off the data provided
--- numbers are extremely close so we will keep them - The Second includes "International"  Location
-
+-- numbers are extremely close so we will keep them - The Second includes "International"  Locatio
 
 --Select SUM(new_cases) as total_cases, SUM(cast(new_deaths as int)) as total_deaths, SUM(cast(new_deaths as int))/SUM(New_Cases)*100 as DeathPercentage
 --From PortfolioProject..CovidDeaths
@@ -27,13 +23,13 @@ order by 1,2
 --order by 1,2
 
 
--- 2. Death count per location
+-- 2. Filtering out data that inner values of other data
 
 -- We take these out as they are not inluded in the above queries and want to stay consistent
 -- European Union is part of Europe
 
 Select location, SUM(cast(new_deaths as int)) as TotalDeathCount
-From PortfolioProject..CovidDeaths
+From SQL_Tutorial..CovidDeaths$
 --Where location like '%states%'
 Where continent is null 
 and location not in ('World', 'European Union', 'International')
@@ -41,20 +37,24 @@ Group by location
 order by TotalDeathCount desc
 
 
--- 3. Case count and rate per location
+-- 3. Countries' case count and infection rate
 
-Select Location, Population, MAX(total_cases) as HighestInfectionCount,  Max((total_cases/population))*100 as PercentPopulationInfected
-From PortfolioProject..CovidDeaths
---Where location like '%states%'
+Select Location, Population, ISNULL(MAX(total_cases),0) as HighestInfectionCount,  
+ISNULL(Max((total_cases/population))*100,0) as PercentPopulationInfected
+From SQL_Tutorial..CovidDeaths$
+Where location IS NOT NULL AND Population IS NOT NULL
 Group by Location, Population
 order by PercentPopulationInfected desc
 
 
--- 4. Case count and rate per location, date
+-- 4. Countries' total case counts and infection rate per date
 
 
-Select Location, Population,date, MAX(total_cases) as HighestInfectionCount,  Max((total_cases/population))*100 as PercentPopulationInfected
-From PortfolioProject..CovidDeaths
---Where location like '%states%'
+Select Location, Population, date, ISNULL(MAX(total_cases),0) as HighestInfectionCount,  
+ISNULL(Max((total_cases/population))*100,0) as PercentPopulationInfected
+From SQL_Tutorial..CovidDeaths$
+Where location is not null and Population is not null
 Group by Location, Population, date
 order by PercentPopulationInfected desc
+
+-- B. Copying data to 4 separate excel files and import to Tableau later
