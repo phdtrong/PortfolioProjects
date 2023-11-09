42ed5a3d57203cb2442ab57c50f35b8e7f5aaec7
Trong D. Pham <70780829+phdtrong@users.noreply.github.com>
2023-11-08 17:48:22 -0800

    Update etl_airflow.sh

--- project13/etl_airflow.sh
+++ project13/etl_airflow.sh
@@ -1,1 +1,11 @@
+# move the python task to DAGS list
+cp ETL_Server_Access_Log_Processing.py $AIRFLOW_HOME/dags
 
+# list out existing(submitted) dags
+airflow dags list
+
+# verify the my-first-dag is in the output list
+irflow dag list | grep "ETL_Server_Access_Log_Processing"
+
+# run the "my-first-dag"
+airflow tasks list ETL_Server_Access_Log_Processing
