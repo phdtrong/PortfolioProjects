3542589973aa6b45e2efd5e4510c1c3078713941
Trong D. Pham <70780829+phdtrong@users.noreply.github.com>
2023-11-08 17:48:05 -0800

    Update ETL_Server_Access_Log_Processing.py

--- project13/ETL_Server_Access_Log_Processing.py
+++ project13/ETL_Server_Access_Log_Processing.py
@@ -64,18 +64,3 @@ load = BashOperator(
 
 # task pipeline
 extract >> transform >> load
-
-
-
-
-# move the python task to DAGS list
-cp ETL_Server_Access_Log_Processing.py $AIRFLOW_HOME/dags
-
-# list out existing(submitted) dags
-airflow dags list
-
-# verify the my-first-dag is in the output list
-irflow dag list | grep "ETL_Server_Access_Log_Processing"
-
-# run the "my-first-dag"
-airflow tasks list ETL_Server_Access_Log_Processing
